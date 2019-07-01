500989b2f8e59afeeabfb5133009835e5f85f76c
Tobi <todito.gaming@gmx.de>
2019-07-01 08:45:55 +0200

    hi

--- Welt.java
+++ Welt.java
@@ -3,52 +3,56 @@ public class Welt
     Feldinhalt [][] matrix;
     int feld;
     Mine mine;
-    
+
     public Welt(int Feldgrösse,int AnzahlMinen)
     {
-         feld = Feldgrösse;
-         matrix = new Feldinhalt[Feldgrösse][Feldgrösse];
-         mienenSetzen(AnzahlMinen);
+        feld = Feldgrösse;
+        matrix = new Feldinhalt[Feldgrösse][Feldgrösse];
+        mienenSetzen(AnzahlMinen);
     }
-    
+
     public void mienenSetzen(int AnzahlMinen){
-        for(int i = 0;i < AnzahlMinen;i++){
+        for(int i = 0;i < AnzahlMinen;i++){           
             int a = (int) Math.random() * feld;
             int b = (int) Math.random() * feld;
-            matrix[a][b] = new Mine();
-        }        
+            if(matrix[a][b]==null){
+                matrix[a][b] = new Mine();
+            } else{ 
+                i--;
+            }
+        }
         zahlenSetzen();
     }
-    
+
     public void zahlenSetzen(){
         for(int i = 0;i < feld; i++){
             for(int k = 0; k < feld; i++){
                 if(matrix[i][k] == mine){
                     if(matrix[i-1][k]==null){
-                    matrix[i-1][k] = new Zahl();
-                }
+                        matrix[i-1][k] = new Zahl();
+                    }
                     if(matrix[i-1][k+1]==null){
-                matrix[i-1][k+1] = new Zahl();
+                        matrix[i-1][k+1] = new Zahl();
+                    }
+                    if(matrix[i][k+1]==null){        
+                        matrix[i][k+1] = new Zahl();
+                    }
+                    if(matrix[i+1][k+1]==null){
+                        matrix[i+1][k+1] = new Zahl();
+                    }
+                    if(matrix[i+1][k]==null){
+                        matrix[i+1][k] = new Zahl();
+                    }
+                    if(matrix[i+1][k-1]==null){   
+                        matrix[i+1][k-1] = new Zahl();
+                    }
+                    if(matrix[i][k-1]==null){
+                        matrix[i][k-1] = new Zahl();
+                    }
+                    if(matrix[i-1][k-1]==null){
+                        matrix[i-1][k-1] = new Zahl();
+                    }
                 }
-                if(matrix[i][k+1]==null){        
-                matrix[i][k+1] = new Zahl();
-                }
-                if(matrix[i+1][k+1]==null){
-                    matrix[i+1][k+1] = new Zahl();
-                }
-                if(matrix[i+1][k]==null){
-                    matrix[i+1][k] = new Zahl();
-                }
-                if(matrix[i+1][k-1]==null){   
-                matrix[i+1][k-1] = new Zahl();
-                }
-                if(matrix[i][k-1]==null){
-                    matrix[i][k-1] = new Zahl();
-                }
-                if(matrix[i][k]==null){
-                    matrix[i-1][k-1] = new Zahl();
-                }
-            }
             }
         }
     }
